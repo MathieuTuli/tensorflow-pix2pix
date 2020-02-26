3ed23d6e355eaf713aa6463947e9bbfb35c034c7
Mathieu Tuli <tuli.mathieu@gmail.com>
2020-02-26 05:47:41 -0500

    Working on predictor

--- src/TFPix2Pix/predictor.py
+++ src/TFPix2Pix/predictor.py
@@ -50,5 +50,5 @@ class Predictor():
                 # getting the pixel values between [0, 1] to plot it.
                 plt.imshow(display_list[i] * 0.5 + 0.5)
                 plt.axis('off')
-            plt.show()
+            plt.savefig('/home/mat/Downloads/test.png')
             return prediction[0]
